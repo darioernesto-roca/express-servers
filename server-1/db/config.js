990700c6343f44159a2f04711269607e4ad49167
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-03-04 23:06:44 -0500

    Creating database connection

---
+++ server-1/db/config.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const user = process.env.DB_USER;
+const host = process.env.DB_HOST;
+const database = process.env.DB_DATABASE;
+const password = process.env.DB_PASSWORD;
+const port = process.env.DB_PORT;
+
+const pool = new Pool({
+    user: user,
+    host: host,
+    database: database,
+    password: password,
+    port: port,
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to the database');
+    release();
+});
+
+module.exports = pool;
--- server-1/index.js
+++ server-1/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./routes');
+const pool = require('./db/config');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -14,6 +15,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/', routes);
+
+pool.connect((err, client, release) => {
+    if (err) {
+        return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to the database');
+    release();
+});
 
 app.listen(port, () => {
     console.log(`🌎Server is running on http://localhost:${port}`);
