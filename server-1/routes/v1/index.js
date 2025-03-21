03c82ea5c3e144227f21a639a28a671a47a95e3c
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-03-20 23:25:09 -0500

    Server 1: Creating authentication system

--- server-1/routes/v1/index.js
+++ server-1/routes/v1/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 const path = require('path');
+const authRoutes = require('./auth');
+
+// API V1 Auth Routes
+router.use('/auth', authRoutes);
 
 // API V1 Home Route
 router.get('/', (req, res) => {
